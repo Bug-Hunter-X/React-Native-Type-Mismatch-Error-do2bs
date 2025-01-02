f4ebdfbfae04a8e18c8fe20efb95c6a7df351190
BugHunter-X <askzenai@gmail.com>
2025-01-02 17:28:05 +0100

    Add TypeMismatchBug.js file

---
+++ TypeMismatchBug.js
@@ -0,0 +1,1 @@
+This error occurs when you use a component that expects a specific prop type, but you provide a value of a different type. For example, if a component expects a number prop, and you pass it a string, you'll encounter a type mismatch error. This is often overlooked when dealing with dynamic data or prop drilling.
